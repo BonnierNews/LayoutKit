13c3c3ae3858818ab633c4cec4145b8efd13b93c
Andre Stenvall <andre.stenvall@expressen.se>
2018-04-11 13:49:21 +0200

    Clean up build script

--- build.sh
+++ build.sh
@@ -6,13 +6,14 @@ alias xcpretty='bundle exec xcpretty'
 
 # Defined default derived data location
 DERIVED_DATA=${1:-/tmp/LayoutKit}
-echo "Derived data location: $DERIVED_DATA";
+echo "Derived data location: $DERIVED_DATA"
 
-set -o pipefail &&
+# Fail on error
+set -e
 
-echo "Run tests on iOS..." &&
-rm -rf $DERIVED_DATA &&
-time xcodebuild clean test \
+echo "Run tests on iOS..."
+rm -rf $DERIVED_DATA
+set -o pipefail && time xcodebuild clean test \
     -project LayoutKit.xcodeproj \
     -scheme LayoutKit-iOS \
     -sdk iphonesimulator11.2 \
@@ -23,12 +24,12 @@ time xcodebuild clean test \
     -destination 'platform=iOS Simulator,name=iPhone 7 Plus,OS=11.2' \
     OTHER_SWIFT_FLAGS='-Xfrontend -debug-time-function-bodies' \
     | tee build.log \
-    | xcpretty &&
-cat build.log | sh debug-time-function-bodies.sh &&
+    | xcpretty
+cat build.log | sh debug-time-function-bodies.sh
 
-echo "Building sample app..." &&
-rm -rf $DERIVED_DATA &&
-time xcodebuild clean build \
+echo "Building sample app..." 
+rm -rf $DERIVED_DATA
+set -o pipefail && time xcodebuild clean build \
     -project LayoutKit.xcodeproj \
     -scheme LayoutKitSampleApp \
     -sdk iphonesimulator11.2 \
@@ -39,16 +40,16 @@ time xcodebuild clean build \
     -destination 'platform=iOS Simulator,name=iPhone 7 Plus,OS=11.2' \
     OTHER_SWIFT_FLAGS='-Xfrontend -debug-time-function-bodies' \
     | tee ../build.log \
-    | xcpretty &&
-cat build.log | sh debug-time-function-bodies.sh &&
+    | xcpretty
+cat build.log | sh debug-time-function-bodies.sh
 
 # Test Cocopods, Carthage, Swift Package Management
 
-echo "Building an iOS empty project with cocoapods..." &&
-rm -rf $DERIVED_DATA &&
-cd Tests/cocoapods/ios &&
-pod install &&
-time xcodebuild clean build \
+echo "Building an iOS empty project with cocoapods..."
+rm -rf $DERIVED_DATA
+cd Tests/cocoapods/ios
+pod install
+set -o pipefail && time xcodebuild clean build \
     -workspace LayoutKit-iOS.xcworkspace \
     -scheme LayoutKit-iOS \
     -sdk iphonesimulator11.2 \
@@ -56,6 +57,6 @@ time xcodebuild clean build \
     -destination 'platform=iOS Simulator,name=iPhone 7,OS=11.2' \
     OTHER_SWIFT_FLAGS='-Xfrontend -debug-time-function-bodies' \
     | tee ../../../build.log \
-    | xcpretty &&
-cd ../../.. &&
+    | xcpretty
+cd ../../.. 
 cat build.log | sh debug-time-function-bodies.sh
