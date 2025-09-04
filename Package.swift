87e9f2a578b33b210d39019ce52a6f848f3ffbd9
Jon Andersson <andersson.jon@gmail.com>
2025-09-04 14:35:37 +0200

    no objc

--- Package.swift
+++ Package.swift
@@ -11,43 +11,6 @@ let package = Package(
         .library(name: "LayoutKit", targets: ["LayoutKit"])
     ],
     targets: [
-        // ObjC-only target
-        .target(
-            name: "LayoutKitObjC",
-            path: "Sources/ObjCSupport",
-            sources: ["Builders"],
-            publicHeadersPath: "Builders"
-        ),
-        // Swift parts of ObjCSupport
-        .target(
-            name: "LayoutKitObjCSwift",
-            dependencies: ["LayoutKitObjC"],
-            path: "Sources/ObjCSupport",
-            sources: [
-                "Internal/ReverseWrappedLayout.swift",
-                "Internal/WrappedLayout.swift",
-                "LOKAlignment.swift",
-                "LOKAnimation.swift",
-                "LOKBaseLayout.swift",
-                "LOKBatchUpdates.swift",
-                "LOKButtonLayout.swift",
-                "LOKButtonLayoutType.swift",
-                "LOKFlexibility.swift",
-                "LOKInsetLayout.swift",
-                "LOKLabelLayout.swift",
-                "LOKLayout.swift",
-                "LOKLayoutArrangement.swift",
-                "LOKLayoutArrangementSection.swift",
-                "LOKLayoutMeasurement.swift",
-                "LOKLayoutSection.swift",
-                "LOKOverlayLayout.swift",
-                "LOKReloadableViewLayoutAdapter.swift",
-                "LOKSizeLayout.swift",
-                "LOKStackLayout.swift",
-                "LOKTextViewLayout.swift"
-            ]
-        ),
-        // Main LayoutKit Swift code
         .target(
             name: "LayoutKit",
             path: "Sources",
